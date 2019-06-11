0a39c3f33b98fcb50051c208c2abe0a1aba175f8
Mari0203 <mari.brands@pm.me>
2019-06-10 19:25:51 -0500

    Updated JS: Added an array and initial random selection set-up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,3 +16,22 @@ var placeList = [
     "Shanghai",
     "Korea"
 ];
+
+// Set # of tries to a variable
+var tryNums = 0;
+
+// Pick a random word from the placeList array.
+var word = placeList[Math.floor(Math.random() * placeList.length)];
+console.log("Randomly selected place: " + word);
+
+// Create an empty array to store answer letters,
+// and fill it with underscore ( _ ) to reflect the # of letters in the answered word.
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = " _ ";
+};
+console.log("The chosen word underscored: " + answerArray);
+
+// Keep track of remaining unanswered letters by setting it to a variable.
+// var unansweredLetters = # of underscores remaining in a word
+
