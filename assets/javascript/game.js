fdc44837ad768eb7eecf5bac7a797dca10560c06
Mari0203 <mari.brands@pm.me>
2019-06-10 19:13:01 -0500

    Updated CSS font styles with CDN links

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,18 @@
-/*   
-*/
+// Create an array of places/cities.
+var placeList = [
+    "Paris",
+    "London",
+    "Istanbul",
+    "Casablanca",
+    "Tokyo",
+    "Hawaii",
+    "India",
+    "Moscow",
+    "Hollywood",
+    "Egypt",
+    "Sydney",
+    "Ireland",
+    "Thai",
+    "Shanghai",
+    "Korea"
+];
